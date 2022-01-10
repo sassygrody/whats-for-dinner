b86ed501490349331189bbd226f6c7aefb18e565
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2022-01-10 17:09:16 -0600

    add helper class for food item and icon

--- app/helpers/food_items_helper.rb
+++ app/helpers/food_items_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module FoodItemsHelper
-  def icon(item)
-    case item.item_type_before_type_cast
+  def icon(food_item)
+    case food_item.item_type_before_type_cast
     when FoodItem.item_types[:vegetable] then "carrot"
     when FoodItem.item_types[:fat] then "cheese"
     when FoodItem.item_types[:protein] then "fish"
@@ -10,5 +10,9 @@ module FoodItemsHelper
     else
       "utensils"
     end
+  end
+
+  def food_item_class_name(food_item)
+    "food-item #{food_item.item_type}"
   end
 end
