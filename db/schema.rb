4654049c3afbe202bc0f66c99096e0254a3da8c7
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2022-02-14 22:44:39 -0600

    Users scaffold for example

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_031045) do
+ActiveRecord::Schema.define(version: 2022_02_15_043158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 2022_02_15_031045) do
   create_table "meals", force: :cascade do |t|
     t.integer "name"
     t.string "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    age { "MyString" }
+  end
+end
