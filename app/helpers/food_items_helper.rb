ba649a68c24af4991df4d4f3527232b9e5fc5ed1
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2022-03-05 21:22:54 -0600

    add back food styles

--- app/helpers/food_items_helper.rb
+++ app/helpers/food_items_helper.rb
@@ -12,7 +12,15 @@ module FoodItemsHelper
     end
   end
 
-  def food_item_class_name(food_item)
-    "food-item #{food_item.item_type}"
+  def food_item_classes(food_item)
+    spacing = " py-1 mb-3 mr-2 flex items-center"
+    case food_item.item_type
+    when "vegetable" then "bg-lime-500 #{spacing}"
+    when "carb" then "bg-yellow-400 #{spacing}"
+    when "protein" then "bg-sky-300 #{spacing}"
+    when "fat" then "bg-fuchsia-300 #{spacing}"
+    else
+      "bg-zinc-300 #{spacing}"
+    end
   end
 end
